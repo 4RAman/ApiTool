bd54b4182acdefa0a63b540b2afbf045c22383ad
Anthony Aman <Anthonyaman@gmail.com>
2018-06-20 12:18:07 -0600

    Cleanup 	modified:   .gitignore 	deleted:    Chosen.py 	deleted:    Toolkit/AdminSearch.py 	deleted:    Toolkit/__pycache__/AdminSearch.cpython-35.pyc 	deleted:    Toolkit/__pycache__/__init__.cpython-35.pyc 	deleted:    Toolkit/search.py 	deleted:    api.json.default 	deleted:    api.py 	deleted:    defaults/.api.json.default.swp 	modified:   defaults/api.json.default 	modified:   defaults/headers.json.default 	deleted:    headers.json.default 	deleted:    searchLoop.py 	deleted:    setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir /usr/bin/ApiTool;
-sudo cp -R ../ApiTool /usr/bin/ApiTool;
-sudo cp /usr/bin/ApiTool/headers.json.default /usr/bin/ApiTool/headers.json
-sudo cp /usr/binApiTool/api.json.default /usr/bin/ApiTool/api.json
